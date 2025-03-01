3a6640f7b9dcf9329b6eaba0f1c9033acd04e766
EduConnect-Hub <maheshsharma3317js@gmail.com>
2025-03-01 23:42:26 +0530

    Added content to App.js

---
+++ App.js
@@ -0,0 +1,29 @@
+const heading = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "Heading 1"
+);
+
+const heading2 = React.createElement(
+    "h2",
+    {
+        id: "title",
+    },
+    "Heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+    },
+    [heading,heading2]
+);
+
+const root = ReactDOM.creatRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+//async defer
+root.render(container);
