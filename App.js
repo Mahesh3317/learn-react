9fd7143686a5f645218ef7a28693d990495ce5f8
EduConnect-Hub <maheshsharma3317js@gmail.com>
2025-03-05 08:25:37 +0530

    Added App1.js with second commit

--- App.js
+++ App.js
@@ -1,35 +1,44 @@
 
+// //  <div class="header">
+// //  <h1>Namaste React</h1>
+// //  <ul>
+// //      <li>About us</li>
+// //      <li>Support</li>
+// //      <li>Home</li>                   
+// //  </ul>
+// // </div> 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+// const heading = React.createElement(
+//     "h1",//THIS IS CALLED AS REACT ELEMENT
+//     {
+//         id: "title",
+//     },
+//     "Heading 1"
+// );
 
-const heading = React.createElement(
-    "h1",
-    {
-        id: "title",
-    },
-    "Heading 1"
-);
+// const heading2 = React.createElement(
+//     "h2",
+//     {
+//         id: "title",
+//         key:"h1",
+//     },
+//     "Heading 2"
+// );
 
-const heading2 = React.createElement(
-    "h2",
-    {
-        id: "title",
-        // hey :"hey",
-    },
-    "Heading 2"
-);
+// //React.createElement => Object => HTML(DOM)
 
-const container = React.createElement(
-    "div",
-    {
-        id: "container",
-    },
-    [heading,heading2]
-);
+// const container = React.createElement(
+//     "div",
+//     {
+//         id: "container",
+//     },
+//     [heading,heading2]
+// );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//passing a react element inside the root
-//async defer
-root.render(container);
+// //passing a react element inside the root
+// //async defer
+// root.render(container);
--- App1.js
+++ App1.js
@@ -10,37 +10,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "div",
-    {
-        id: "title",
-    },
-[
-    React.createElement(
-        "h1",
-        {
-            id: "title",
-        },
-        "Heading 1"
-    ),
+//jsx ?
+// jsx => React.createElement => Object =>HTML(DOM)
 
-    React.createElement(
-        "h1",
-        {
-            id: "title",
-        },
-        "Heading 1"
-    ),
+// const heading3 =  <h1 id="title">Namaste react</h1>
+//for multiple lines we use ()
+
+//React Element
+const heading2 =() => (        
+    <h1 id="title" key="h2">  
+    Namaste react1
+    </h1>                //JSX EXPRESSION
+)
+
+// React Component
+//-1.Functional -New
+//-2.Class Based Component -Old
+//Name of Component start with capital latter - it not mandatory
+
+// const HeaderComponent = () =>{
+//     // return <h1>Namaste react functional Component</h1>
+//     return (
+//     <div>
+//     <h1>Namaste react functional Component</h1>
+//     </div>
+//     );
+// }
+
+
+const HeaderComponent = () =>(
+    <div>
+    {/* {heading2} //for react element  */}
+    <heading2/>
+     {/* for functional component AS well we can also write like this {heading2()}  */}
     
-    React.createElement(
-        "h1",
-        {
-            id: "title",
-        },
-        "Heading 1"
-    )
-]
-);
+    <h1>Namaste react functional Component</h1>
+    </div>
+    );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeaderComponent/>);
